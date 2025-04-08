559d4a54af30f35e70f6a3d2e57824b7c5504789
Antony Wu <sunnydelight@umich.edu>
2025-04-08 02:53:17 +0000

    Third check in

--- octofit-tracker/backend/octofit_tracker/settings.py
+++ octofit-tracker/backend/octofit_tracker/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-i#t30%)^ajs7x3at^*91fegsq8yz=xo)j05527r*jc_8%i1w4_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ["localhost", "127.0.0.1", "[REPLACE-THIS-WITH-YOUR-CODESPACE-NAME]-8000.app.github.dev"]
 
 
 # Application definition
